59d40f28b68f6a5750655fc5acb34802820140a7
tora262 <tth.hust@gmail.com>
2023-10-05 16:20:57 +0700

    Session 5 - demo String Builder

--- Session5Demo/src/StringBuilderDemo.java
+++ Session5Demo/src/StringBuilderDemo.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class StringBuilderDemo {
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("Hello");
+        stringBuilder.append(" ");
+        stringBuilder.append("Java");
+        stringBuilder.insert(6, "Python, ");
+        stringBuilder.delete(6, 14);
+        System.out.println(stringBuilder.reverse().reverse().toString());
+    }
 }
